eecf632d578d23c1a26ee41e521a691dabd32337
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-01-23 08:24:04 -0600

    Add navLinks and mobileNavLinks

---
+++ src/assets/data/links.js
@@ -0,0 +1,16 @@
+export const navLinks = [
+   {id: Number(1001), name: 'Home', path: '/'},
+   {id: Number(1002), name: 'About', path: '/about'},
+   {id: Number(1003), name: 'Services', path: '/services'},
+   {id: Number(1004), name: 'Contact', path: '/contact'},
+   {id: Number(1005), name: 'Get Started', path: '/sign-in'}
+];
+
+
+export const mobileNavLinks = [
+   {id: Number(2001), name: 'Home', path: '/'},
+   {id: Number(2002), name: 'About', path: '/about'},
+   {id: Number(2003), name: 'Services', path: '/services'},
+   {id: Number(2004), name: 'Contact', path: '/contact'},
+   {id: Number(2005), name: 'Get Started', path: '/sign-in'}
+];
